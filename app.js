2ab7ae8606a7b465ab4746e5db689ea73b643bda
Armas <Armas.Nevolainen@metropolia.fi>
2024-10-07 13:41:43 +0300

    Done

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const {requestLogger,unknownEndpoint,errorHandler} = require("./middleware/customMiddleware");
+const {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+} = require("./middleware/customMiddleware");
 const todoTaskRouter = require("./routers/todoTaskRouter");
 const userRouter = require("./routers/userRouter");
 const tourRouter = require("./routers/tourRouter");
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./swagger.json");
 
 // express app
 const app = express();
@@ -23,7 +29,7 @@ app.get("/", (req, res) => res.send("API Running!"));
 app.use("/api/todoTasks", todoTaskRouter);
 app.use("/api/users", userRouter);
 app.use("/api/tours", tourRouter);
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
